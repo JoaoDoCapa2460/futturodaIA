d6dfda55a9224a0ef8a3ac81b8fc94b6d78844c5
JoaoDoCapa2460 <145778748+JoaoDoCapa2460@users.noreply.github.com>
2025-08-28 11:36:53 -0300

    .

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+cost perguntas = [
+{
+    enunciado:
+    "Oque voce acha sobre o chat GBT?"
+    alternativas:"bom", "ruim"
+
+    enunciado:
+voce acha que ela vai mudar o mundo?
+alternativas:{
+    "muito provavel"
+    "tenho certeza que não"
+
+    enunciado: {
+        "O chatGBT pode acabar com o mundo?"
+        alternativas
+        "sim" "não"
+
+    }
+
+}
+
